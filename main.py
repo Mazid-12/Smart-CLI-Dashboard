dde7dcb476bfa51e3acd3386a996e40e08c0f18f
Mazid <mazidyerima2@gmail.com>
2026-05-20 18:42:32 +0000

    Just created the menu function which prints the menu

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Starting a new project")
+def print_menu():
+    print("===== MENU =====")
+    print("1. View profile \n2. See Weather \n3. Convert currency \n4. Search history \n0. Exit")
+print_menu()
