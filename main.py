9f16003fd08c402673cbc0b3f0e1c77a4bd36f25
Mazid <mazidyerima2@gmail.com>
2026-05-21 08:12:41 +0000

    I just created a new function sign_up() for new users to sign_up their accounts. So it stores the name and the date the account was created

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from User import *
 import json
+from datetime import datetime
 
 def print_menu():
     print("===== MENU =====")
     print("1. View profile \n2. See Weather \n3. Convert currency \n4. Search history \n0. Exit")
 
-
 def create_file():
     user_list = []
     with open("user.json", "w") as file:
         json.dump(user_list, file, indent=4)
-create_file()
+
+def sign_up():
+    name = input("Enter your name: ")
+    long_date = datetime.now()
+    date_creation = long_date.strftime("%Y-%m-%d")
+    user = User.create_user(name, date_creation)
+    user_dictionary = {'name' : name,
+                       'creation_date' : date_creation}
+    with open("user.json", 'r') as file:
+        user_data = json.load(file)
+        
+    user_data.append(user_dictionary)
+
+    with open("user.json", "w") as file:
+        json.dump(user_data, file, indent=4)
+
+
+
 
 
 
